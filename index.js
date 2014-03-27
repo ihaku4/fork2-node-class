14b1fdc9eb07bb94ffb61b92111369a93a55604d
Durrrr <ihaku4@gmail.com>
2014-03-27 18:12:45 +0800

    move instance methods to prototype

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 (function() {
     function Class(props) {
         function Constructor() {
-            for (var key in props) {
-                if (typeof props[key] !== "function") {
-                    continue;
-                }
-                if (key === "initialize") {
-                    props[key].apply(this, arguments);
-                } else {
-                    this[key] = props[key];
-                }
+            if (props.hasOwnProperty("initialize")) {
+                props["initialize"].apply(this, arguments);
+            }
+        }
+        for (var key in props) {
+            if (key !== "initialize" && typeof props[key] === "function") {
+                Constructor.prototype[key] = props[key];
             }
         }
         return Constructor;
