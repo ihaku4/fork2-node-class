030b00db3a1a56a284fa8485b0844a183a7476a4
Durrrr <ihaku4@gmail.com>
2014-03-28 16:53:10 +0800

    methods-inheritance

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 (function() {
-    function Class(props) {
+    function Class(props, SuperConstructor) {
         function Constructor() {
             if (props.hasOwnProperty("initialize")) {
                 props["initialize"].apply(this, arguments);
             }
+            this.constructor = arguments.callee;
         }
         for (var key in props) {
             if (key !== "initialize" && typeof props[key] === "function") {
                 Constructor.prototype[key] = props[key];
             }
+        }
+        if (SuperConstructor) {
+            Constructor.prototype = new SuperConstructor();
         }
         return Constructor;
     }
